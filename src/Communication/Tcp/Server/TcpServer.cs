0e2eae19d7fce4330f0b6b1288eb5f0e7c12128e
hwanyong.lee <dlghksdyd@gmail.com>
2025-09-01 21:02:53 +0900

    구조 일부 수정

--- src/Communication/Tcp/Server/TcpServer.cs
+++ src/Communication/Tcp/Server/TcpServer.cs
@@ -453,4 +453,13 @@ namespace HYSoft.Communication.Tcp.Server
         }
     }
 
+    internal sealed class TcpClientContext(Socket socket)
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public Socket Socket { get; } = socket ?? throw new ArgumentNullException(nameof(socket));
+        public EndPoint? RemoteEndPoint { get; } = socket.RemoteEndPoint;
+        public DateTime ConnectedAtUtc { get; } = DateTime.UtcNow;
+        public CancellationTokenSource Cts { get; } = new();
+        public SemaphoreSlim SendLock { get; } = new(1, 1);
+    }
 }
