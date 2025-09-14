02badcc0d910f8c62ac07b35b02a826f9af7a786
hwanyong.lee <dlghksdyd@gmail.com>
2025-09-14 15:24:58 +0900

    IconExtension 구현 중

---
+++ src/Presentation.Styles/Icons/IconExtension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace HYSoft.Presentation.Styles.Icons
+{
+    [MarkupExtensionReturnType(typeof(Brush))]
+    public sealed class IconExtension : MarkupExtension
+    {
+        public IconExtension()
+        {
+        }
+
+        public IconExtension(object itemKey) => ItemKey = itemKey;
+
+        [ConstructorArgument("itemKey")]
+        public object ItemKey { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var palette = IconCollection.Current;
+
+            return palette.GetBrush(ItemKey);
+        }
+    }
+
+    public class IconCollection : Freezable
+    {
+        private Dictionary<object, Brush>? _map;
+
+        private static IconCollection? _current;
+        public static IconCollection Current => _current = Initialize();
+
+        public Brush GetBrush(object key)
+        {
+            return _map is null ? new SolidColorBrush(Colors.Transparent) : _map[key];
+        }
+
+        private static IconCollection Initialize()
+        {
+            if (_current is not null) return _current;
+
+            //var p = new IconCollection
+            //{
+            //    _map = ColorGenerator.Generate()
+            //};
+            //return p;
+            return new IconCollection();
+        }
+
+        protected override Freezable CreateInstanceCore() => new IconCollection();
+    }
+}
